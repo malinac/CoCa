81c63df67a1ca75aa790764fe659dd3b54e692e4
malinac <malina.catanoiu@gmail.com>
2019-01-27 19:57:21 +0200

    added calendar sizing

---
+++ Extension/popup/calendar_sizing.js
@@ -0,0 +1,15 @@
+function smalldimension() {
+    document.getElementById("calendar").style.transform = "scale(0.6)"
+    
+}
+
+function mediumdimension() {
+    document.getElementById("calendar").style.transform = "scale(0.75)"
+}
+
+function largedimension() {
+    document.getElementById("calendar").style.transform = "scale(1.1)"
+}
+function xlargedimension() {
+    document.getElementById("calendar").style.transform = "scale(1.2)"
+}
