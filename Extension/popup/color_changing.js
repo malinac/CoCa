ca67d66f4286c799749c0cd818cd5747830f3146
malinac <malina.catanoiu@gmail.com>
2019-01-27 20:35:50 +0200

    updated

---
+++ Extension/popup/color_changing.js
@@ -0,0 +1,14 @@
+function bluetheme() {
+    document.getElementById("ls").style.background = "#0087bd"
+
+}
+function greentheme() {
+    document.getElementById("ls").style.background = "#488615"
+
+}
+function yellowtheme() {
+    document.getElementById("ls").style.background = "#cd3700"
+}
+function redtheme() {
+    document.getElementById("ls").style.background = "#ec364f"
+}
