86c4b85a5f30ca3397d00e2e8d403863f33fcf44
malinac <malina.catanoiu@gmail.com>
2019-01-29 20:55:36 +0200

    updated

--- Extension/popup/color_changing.js
+++ Extension/popup/color_changing.js
@@ -1,25 +1,32 @@
 function bluetheme() {
-    document.getElementById("ls").style.background = "#0087bd"
-    localStorage.setItem("color", "#0087bd")
+    document.getElementById("ls").style.background = "#0087bd";
+    document.getElementById("logo").style.color ="#0087bd";
+    localStorage.setItem("color", "#0087bd");
 
 }
 function greentheme() {
     document.getElementById("ls").style.background = "#488615"
+    document.getElementById("logo").style.color = "#488615";
     localStorage.setItem("color", "#488615")
 
 }
 function yellowtheme() {
-    document.getElementById("ls").style.background = "#cd3700"
+    document.getElementById("ls").style.background = "#cd3700";
+    document.getElementById("logo").style.color = "#cd3700";
     localStorage.setItem("color","#cd3700" )
 }
 function redtheme() {
-    document.getElementById("ls").style.background = "#ec364f"
+    document.getElementById("ls").style.background = "#ec364f";
+    document.getElementById("logo").style.color = "#ec364f";
     localStorage.setItem("color", "#ec364f")
 }
 function getColor(){
     var select = document.getElementById("ls")
     select.value = localStorage.getItem("color");
     select.style.backgroundColor = select.value;
+    var select2 = document.getElementById("logo");
+    select2.value = localStorage.getItem("color");
+    select2.style.color = select2.value;
 }
 
 window.onload = getColor()
---
+++ Extension/popup/getToday.js
@@ -0,0 +1,35 @@
+
+function getToday() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth(); //January is 0!
+
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+
+    var today = dd + '/' + mm + '/' + yyyy;
+    document.getElementById('today').innerHTML = today;
+    return today
+    console.log(typeof(today))
+}
+
+function getFormattedDate(date) {
+    return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+}
+
+function getTodaysEvents(){
+    today = getToday()
+    todayFormatted = getFormattedDate(today);
+    eventList = JSON.parse(localStorage.getItem("calendar-events"), (key, value)=>{
+        var node = document.createElement("LI");                 // Create a <li> node
+        var textnode = document.createTextNode(value);         // Create a text node
+        node.appendChild(textnode);                              // Append the text to <li>
+        document.getElementById("curr-ev").appendChild(node);
+})
+
+}
+window.onload =getToday()
+window.onload=getTodaysEvents()
+
