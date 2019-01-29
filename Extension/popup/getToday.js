590835eec89ba3ed6424aea188850e6a424f1045
malinac <malina.catanoiu@gmail.com>
2019-01-29 21:36:56 +0200

    coca displays today's events

--- Extension/popup/getToday.js
+++ Extension/popup/getToday.js
@@ -11,9 +11,25 @@ function getToday() {
 
     var today = dd + '/' + mm + '/' + yyyy;
     document.getElementById('today').innerHTML = today;
-    return today
-    console.log(typeof(today))
+    t = today.toString();
+    eventList = JSON.parse(localStorage.getItem("calendar-events"))
+    if(eventList[t].length>0){
+        for(i=0; i<=eventList[t].length-1; i++){
+            var node = document.createElement("LI");
+            var textnode = document.createTextNode(eventList[t][i]);
+            node.appendChild(textnode);
+            document.getElementById("curr-ev").appendChild(node);
+        }
+    }
+    else {
+        var node = document.createElement("LI");                 
+        var textnode = document.createTextNode("Nothing today");
+        node.appendChild(textnode);
+        document.getElementById("curr-ev").appendChild(node);
+    }
 }
+
+
 
 
 window.onload =getToday()
