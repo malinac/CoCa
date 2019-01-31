48a3232313d38cfeb93291103c392a72606d8be5
Andreia <andreeea247@yahoo.com>
2019-01-31 10:04:18 +0200

    small update

--- Extension/popup/getToday.js
+++ Extension/popup/getToday.js
@@ -12,10 +12,10 @@ function getToday() {
     var today = dd + '/' + mm + '/' + yyyy;
     document.getElementById('today').innerHTML = today;
     t = today.toString();
-    eventList = JSON.parse(localStorage.getItem("calendar-events"));
-    if(eventList[t].length>0){
-        for(i=0; i<=eventList[t].length-1; i++){
-            var node = document.createElement("LI");
+    eventList = JSON.parse(localStorage.getItem("calendar-events")); 
+    if(eventList[t].length > 0) { 
+        for(i=0; i<=eventList[t].length-1; i++){ 
+            var node = document.createElement("LI"); 
             var textnode = document.createTextNode(eventList[t][i]);
             node.appendChild(textnode);
             document.getElementById("curr-ev").appendChild(node);
@@ -32,6 +32,6 @@ function getToday() {
 
 
 
-window.onload =getToday()
+window.onload = getToday()
 
 
