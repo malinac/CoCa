7f7770d8f49509164c64041df3a20ab1ddb7ddff
malinac <malina.catanoiu@gmail.com>
2019-02-10 22:34:57 +0200

    .

--- Extension/popup/widgetColor.js
+++
@@ -1,7 +0,0 @@
-function getColor(){
-    var select = document.getElementById("logo")
-    select.value = localStorage.getItem("color");
-    select.style.backgroundColor = select.value;
-}
-
-window.onload = getColor()
