a4dbf73591a4d51164eb986ad499e8fd6c729840
malinac <malina.catanoiu@gmail.com>
2019-02-11 00:04:39 +0200

    .

--- Extension/popup/c1.js
+++ Extension/popup/c1.js
@@ -1,93 +1,250 @@
-let today = new Date();
-let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
-let selectYear = document.getElementById("year");
-let selectMonth = document.getElementById("month");
+const AVAILABLE_WEEK_DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const localStorageName = 'calendar-events';
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-const AVAILABLE_WEEK_DAYS = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY']
-let monthAndYear = document.getElementById("monthAndYear");
-showCalendar(currentMonth, currentYear);
-function getFormattedDate(date) {
-    return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
-}
 
+class CALENDAR {
+    constructor(options) {
+        this.options = options;
+        this.elements = {
+            days: this.getFirstElementInsideIdByClassName('calendar-days'),
+            week: this.getFirstElementInsideIdByClassName('calendar-week'),
+            month: this.getFirstElementInsideIdByClassName('calendar-month'),
+            year: this.getFirstElementInsideIdByClassName('calendar-current-year'),
+            eventList: this.getFirstElementInsideIdByClassName('current-day-events-list'),
+            eventField: this.getFirstElementInsideIdByClassName('add-event-day-field'),
+            eventAddBtn: this.getFirstElementInsideIdByClassName('add-event-day-field-btn'),
+            currentDay: this.getFirstElementInsideIdByClassName('calendar-left-side-day'),
+            currentWeekDay: this.getFirstElementInsideIdByClassName('calendar-left-side-day-of-week'),
+            prevYear: this.getFirstElementInsideIdByClassName('calendar-change-year-slider-prev'),
+            nextYear: this.getFirstElementInsideIdByClassName('calendar-change-year-slider-next')
+        };
 
+        this.eventList = JSON.parse(localStorage.getItem(localStorageName)) || {};
 
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    showCalendar(currentMonth, currentYear);
-}
-
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    showCalendar(currentMonth, currentYear);
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    showCalendar(currentMonth, currentYear);
-}
-
-function showDay(d, i) {
-    document.getElementById('dayNr').innerHTML = d;
-    document.getElementById('dayName').innerHTML = AVAILABLE_WEEK_DAYS[i];
-}
-function showCalendar(month, year) {
-
-    let firstDay = (new Date(year, month)).getDay();
-    let daysInMonth = 32 - new Date(year, month, 32).getDate();
-
-    let tbl = document.getElementById("calendar-body"); // body of the calendar
-
-    // clearing all previous cells
-    tbl.innerHTML = "";
-
-    // filing data about month and in the page via DOM.
-    monthAndYear.innerHTML = months[month] + " " + year;
-    selectYear.value = year;
-    selectMonth.value = month;
-
-    // creating all cells
-    let date = 1;
-    for (let i = 0; i < 6; i++) {
-        // creates a table row
-        let row = document.createElement("tr");
-
-        //creating individual cells, filing them up with data.
-        for (let j = 0; j < 7; j++) {
-            if (i === 0 && j < firstDay) {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode("");
-                cell.classList.add("unavailable-days");
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-            }
-            else if (date > daysInMonth) {
-                break;
-            }
-
-            else {
-                let cell = document.createElement("td");
-                let cellText = document.createTextNode(date);
-                if (date === today.getDate() && year === today.getFullYear() && month === today.getMonth()) {
-                    cell.classList.add("current-day-now");
-                    document.getElementById('dayNr').innerHTML = date;
-                    document.getElementById('dayName').innerHTML = AVAILABLE_WEEK_DAYS[j];
-                } // color today's date
-                cell.classList.add('cursor-pointer');
-                cell.appendChild(cellText);
-                row.appendChild(cell);
-                date++;
-            }
-
-
-        }
-
-        tbl.appendChild(row); // appending each row into calendar body.
+        this.date = +new Date();
+        this.options.maxDays = 37;
+        this.init();
     }
 
+// App methods
+    init() {
+        if (!this.options.id) return false;
+        this.eventsTrigger();
+        this.drawAll();
+    }
+
+    // draw Methods
+    drawAll() {
+        this.drawWeekDays();
+        this.drawMonths();
+        this.drawDays();
+        this.drawYearAndCurrentDay();
+        this.drawEvents();
+
+    }
+
+    drawEvents() {
+        let calendar = this.getCalendar();
+        let eventList = this.eventList[calendar.active.formatted] || ['No events available'];
+        let eventTemplate = "";
+        eventList.forEach(item => {
+            eventTemplate += `<li>${item}</li>`;
+        });
+
+        this.elements.eventList.innerHTML = eventTemplate;
+    }
+
+    drawYearAndCurrentDay() {
+        let calendar = this.getCalendar();
+        this.elements.year.innerHTML = calendar.active.year;
+        this.elements.currentDay.innerHTML = calendar.active.day;
+        this.elements.currentWeekDay.innerHTML = AVAILABLE_WEEK_DAYS[calendar.active.week];
+    }
+
+    drawDays() {
+        let calendar = this.getCalendar();
+
+        let latestDaysInPrevMonth = this.range(calendar.active.startWeek).map((day, idx) => {
+            return {
+                dayNumber: this.countOfDaysInMonth(calendar.pMonth) - idx,
+                month: new Date(calendar.pMonth).getMonth(),
+                year: new Date(calendar.pMonth).getFullYear(),
+                currentMonth: false
+            }
+        }).reverse();
+
+
+        let daysInActiveMonth = this.range(calendar.active.days).map((day, idx) => {
+            let dayNumber = idx + 1;
+            let today = new Date();
+            return {
+                dayNumber,
+                today: today.getDate() === dayNumber && today.getFullYear() === calendar.active.year && today.getMonth() === calendar.active.month,
+                month: calendar.active.month,
+                year: calendar.active.year,
+                selected: calendar.active.day === dayNumber,
+                currentMonth: true
+            }
+        });
+
+
+        let countOfDays = this.options.maxDays - (latestDaysInPrevMonth.length + daysInActiveMonth.length);
+        let daysInNextMonth = this.range(countOfDays).map((day, idx) => {
+            return {
+                dayNumber: idx + 1,
+                month: new Date(calendar.nMonth).getMonth(),
+                year: new Date(calendar.nMonth).getFullYear(),
+                currentMonth: false
+            }
+        });
+
+        let days = [...latestDaysInPrevMonth, ...daysInActiveMonth, ...daysInNextMonth];
+
+        days = days.map(day => {
+            let newDayParams = day;
+            let formatted = this.getFormattedDate(new Date(`${Number(day.month) + 1}/${day.dayNumber}/${day.year}`));
+            newDayParams.hasEvent = this.eventList[formatted];
+            return newDayParams;
+        });
+
+        let daysTemplate = "";
+        days.forEach(day => {
+            daysTemplate += `<li class="${day.currentMonth ? '' : 'another-month'}${day.today ? ' active-day ' : ''}${day.selected ? 'selected-day' : ''}${day.hasEvent ? ' event-day' : ''}" data-day="${day.dayNumber}" data-month="${day.month}" data-year="${day.year}"></li>`
+        });
+
+        this.elements.days.innerHTML = daysTemplate;
+    }
+
+    drawMonths() {
+        let availableMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        let monthTemplate = "";
+        let calendar = this.getCalendar();
+        availableMonths.forEach((month, idx) => {
+            monthTemplate += `<li class="${idx === calendar.active.month ? 'active' : ''}" data-month="${idx}">${month}</li>`
+        });
+
+        this.elements.month.innerHTML = monthTemplate;
+    }
+
+    drawWeekDays() {
+        let weekTemplate = "";
+        AVAILABLE_WEEK_DAYS.forEach(week => {
+            weekTemplate += `<li>${week.slice(0, 3)}</li>`
+        });
+
+        this.elements.week.innerHTML = weekTemplate;
+    }
+
+    // Service methods
+    eventsTrigger() {
+        this.elements.prevYear.addEventListener('click', e => {
+            let calendar = this.getCalendar();
+            this.updateTime(calendar.pYear);
+            this.drawAll()
+        });
+
+        this.elements.nextYear.addEventListener('click', e => {
+            let calendar = this.getCalendar();
+            this.updateTime(calendar.nYear);
+            this.drawAll()
+        });
+
+        this.elements.month.addEventListener('click', e => {
+            let calendar = this.getCalendar();
+            let month = e.srcElement.getAttribute('data-month');
+            if (!month || calendar.active.month == month) return false;
+
+            let newMonth = new Date(calendar.active.tm).setMonth(month);
+            this.updateTime(newMonth);
+            this.drawAll()
+        });
+
+
+        this.elements.days.addEventListener('click', e => {
+            let element = e.srcElement;
+            let day = element.getAttribute('data-day');
+            let month = element.getAttribute('data-month');
+            let year = element.getAttribute('data-year');
+            if (!day) return false;
+            let strDate = `${Number(month) + 1}/${day}/${year}`;
+            this.updateTime(strDate);
+            this.drawAll()
+        });
+
+
+        this.elements.eventAddBtn.addEventListener('click', e => {
+            let fieldValue = this.elements.eventField.value;
+            if (!fieldValue) return false;
+            let dateFormatted = this.getFormattedDate(new Date(this.date));
+            if (!this.eventList[dateFormatted]) this.eventList[dateFormatted] = [];
+            this.eventList[dateFormatted].push(fieldValue);
+            localStorage.setItem(localStorageName, JSON.stringify(this.eventList));
+            this.elements.eventField.value = '';
+            this.drawAll()
+        });
+
+
+    }
+
+
+    updateTime(time) {
+        this.date = +new Date(time);
+    }
+
+    getCalendar() {
+        let time = new Date(this.date);
+
+        return {
+            active: {
+                days: this.countOfDaysInMonth(time),
+                startWeek: this.getStartedDayOfWeekByTime(time),
+                day: time.getDate(),
+                week: time.getDay(),
+                month: time.getMonth(),
+                year: time.getFullYear(),
+                formatted: this.getFormattedDate(time),
+                tm: +time
+            },
+            pMonth: new Date(time.getFullYear(), time.getMonth() - 1, 1),
+            nMonth: new Date(time.getFullYear(), time.getMonth() + 1, 1),
+            pYear: new Date(new Date(time).getFullYear() - 1, 0, 1),
+            nYear: new Date(new Date(time).getFullYear() + 1, 0, 1)
+        }
+    }
+
+    countOfDaysInMonth(time) {
+        let date = this.getMonthAndYear(time);
+        return new Date(date.year, date.month + 1, 0).getDate();
+    }
+
+    getStartedDayOfWeekByTime(time) {
+        let date = this.getMonthAndYear(time);
+        return new Date(date.year, date.month, 1).getDay();
+    }
+
+    getMonthAndYear(time) {
+        let date = new Date(time);
+        return {
+            year: date.getFullYear(),
+            month: date.getMonth()
+        }
+    }
+
+    getFormattedDate(date) {
+        return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+    }
+
+    range(number) {
+        return new Array(number).fill().map((e, i) => i);
+    }
+
+    getFirstElementInsideIdByClassName(className) {
+        return document.getElementById(this.options.id).getElementsByClassName(className)[0];
+    }
 }
+
+
+(function () {
+    new CALENDAR({
+        id: "calendar"
+    })
+});
